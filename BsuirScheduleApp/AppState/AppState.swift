f18bd63b6d05f32827e5257c6091e596cd32d386
Anton Siliuk <siliuk.anton@gmail.com>
2020-08-24 20:50:54 +0300

    Move AllFavoritesScreen in separate file

--- BsuirScheduleApp/AppState/AppState.swift
+++ BsuirScheduleApp/AppState/AppState.swift
@@ -31,27 +31,3 @@ final class AppState: ObservableObject {
     private(set) lazy var allGroups = AllGroupsScreen(requestManager: requestManager, favorites: favorites)
     private(set) lazy var allLecturers = AllLecturersScreen(requestManager: requestManager, favorites: favorites)
 }
-
-final class AllFavoritesScreen: ObservableObject {
-    var isEmpty: Bool { groups.isEmpty && lecturers.isEmpty }
-    @Published private(set) var groups: [AllGroupsScreenGroup] = []
-    @Published private(set) var lecturers: [AllLecturersScreenLecturer] = []
-
-    init(requestManager: RequestsManager, favorites: FavoritesContainer) {
-        self.favorites = favorites
-        self.requestManager = requestManager
-        favorites.$groups.map { $0.value.map(AllGroupsScreenGroup.init) }.assign(to: &$groups)
-        favorites.$lecturers.map { $0.value.map(AllLecturersScreenLecturer.init) }.assign(to: &$lecturers)
-    }
-
-    func screen(for group: AllGroupsScreenGroup) -> ScheduleScreen {
-        .group(group.group, favorites: favorites, requestManager: requestManager)
-    }
-
-    func screen(for lecturer: AllLecturersScreenLecturer) -> ScheduleScreen {
-        .lecturer(lecturer.employee, favorites: favorites, requestManager: requestManager)
-    }
-
-    private let favorites: FavoritesContainer
-    private let requestManager: RequestsManager
-}
