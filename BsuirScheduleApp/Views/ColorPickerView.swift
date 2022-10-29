ae635157f8d430fe0daf5f28f7a2cd545285c02e
Dewerro <funcake0@gmail.com>
2022-10-29 21:01:10 +0300

    Fix: color description in Localizable

--- BsuirScheduleApp/Views/ColorPickerView.swift
+++ BsuirScheduleApp/Views/ColorPickerView.swift
@@ -66,7 +66,7 @@ struct ColorView: View {
             Image(uiImage: iconImage)
                 .resizable()
                 .frame(width: size, height: size)
-            if isOpen { Text("color.\(color.description)") }
+            if isOpen { Text(color.descriptionLocalizedKey ?? "") }
         }.onAppear {
             isOpen = true
         }
