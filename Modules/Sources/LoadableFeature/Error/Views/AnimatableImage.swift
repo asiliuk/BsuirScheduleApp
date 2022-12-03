635486b978d6c0266922c1b811e9552dd966c131
Anton Siliuk <devbudgged@gmail.com>
2022-12-03 22:00:13 +0100

    Fix animating too much

--- Modules/Sources/LoadableFeature/Error/Views/AnimatableImage.swift
+++ Modules/Sources/LoadableFeature/Error/Views/AnimatableImage.swift
@@ -22,8 +22,11 @@ struct AnimatableImage: View {
     var body: some View {
         if #available(iOS 16, *) {
             _Image(systemName: systemName, variableValue: variableValue)
-                .animation(.default.speed(0.3).repeatForever(), value: variableValue)
-                .onAppear { variableValue = 1 }
+                .onAppear {
+                    withAnimation(.default.speed(0.3).repeatForever()) {
+                        variableValue = 1
+                    }
+                }
         } else {
             Image(systemName: systemName)
         }
