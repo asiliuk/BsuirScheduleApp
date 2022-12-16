e1096d5290ac6deb9802f3a52352379a19db0063
Anton Siliuk <devbudgged@gmail.com>
2022-12-16 21:36:33 +0100

    Add onFirstAppear modifier

---
+++ Modules/Sources/BsuirUI/Helpers/FirstAppear.swift
@@ -0,0 +1,24 @@
+/// https://www.swiftjectivec.com/swiftui-run-code-only-once-versus-onappear-or-task/
+import SwiftUI
+
+public extension View {
+    func onFirstAppear(_ action: @escaping () -> ()) -> some View {
+        modifier(FirstAppear(action: action))
+    }
+}
+
+private struct FirstAppear: ViewModifier {
+    let action: () -> ()
+
+    // Use this to only fire your block one time
+    @State private var hasAppeared = false
+
+    func body(content: Content) -> some View {
+        // And then, track it here
+        content.onAppear {
+            guard !hasAppeared else { return }
+            hasAppeared = true
+            action()
+        }
+    }
+}
