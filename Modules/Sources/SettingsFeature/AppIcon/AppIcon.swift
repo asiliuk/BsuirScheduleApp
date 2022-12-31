45ef0f992c29eb8871585dfe5f432e06dac9a2ac
Anton Siliuk <devbudgged@gmail.com>
2022-12-31 19:49:50 +0100

    Use raw value for icon names

--- Modules/Sources/SettingsFeature/AppIcon/AppIcon.swift
+++ Modules/Sources/SettingsFeature/AppIcon/AppIcon.swift
@@ -2,26 +2,26 @@ import Foundation
 import SwiftUI
 
 public enum AppIcon: Hashable {
-    public enum Plain: CaseIterable, Hashable, Identifiable {
+    public enum Plain: String, CaseIterable, Hashable, Identifiable {
         public var id: Self { self }
-        case standard
-        case dark
-        case nostalgia
-        case bsuirDonalds
+        case standard = "AppIcon"
+        case dark = "AppIconDark"
+        case nostalgia = "AppIconNostalgia"
+        case bsuirDonalds = "AppIconBsuirDonalds"
     }
 
-    public enum Symbol: CaseIterable, Hashable, Identifiable {
+    public enum Symbol: String, CaseIterable, Hashable, Identifiable {
         public var id: Self { self }
-        case resist
-        case national
-        case ukrainian
-        case pride
+        case resist = "AppIconResist"
+        case national = "AppIconNational"
+        case ukrainian = "AppIconUkrainian"
+        case pride = "AppIconPride"
     }
 
-    public enum Metal: CaseIterable, Hashable, Identifiable {
+    public enum Metal: String, CaseIterable, Hashable, Identifiable {
         public var id: Self { self }
-        case silver
-        case copper
+        case silver = "AppIconSilver"
+        case copper = "AppIconCopper"
     }
 
     case plain(Plain)
@@ -31,18 +31,14 @@ public enum AppIcon: Hashable {
 
 extension AppIcon {
     init?(name: String) {
-        switch name {
-        case "AppIcon": self = .plain(.standard)
-        case "AppIconDark": self = .plain(.dark)
-        case "AppIconNostalgia": self = .plain(.nostalgia)
-        case "AppIconBsuirDonalds": self = .plain(.bsuirDonalds)
-        case "AppIconResist": self = .symbol(.resist)
-        case "AppIconNational": self = .symbol(.national)
-        case "AppIconUkrainian": self = .symbol(.ukrainian)
-        case "AppIconPride": self = .symbol(.pride)
-        case "AppIconSilver": self = .metal(.silver)
-        case "AppIconCopper": self = .metal(.copper)
-        default: return nil
+        if let plain = Plain(rawValue: name) {
+            self = .plain(plain)
+        } else if let symbol = Symbol(rawValue: name) {
+            self = .symbol(symbol)
+        } else if let metal = Metal(rawValue: name) {
+            self = .metal(metal)
+        } else {
+            return nil
         }
     }
 
@@ -63,24 +59,18 @@ extension AppIcon {
 
     /// Name used to create UIImage for the app icon
     var imageName: String {
-        iconName ?? "AppIcon"
+        switch self {
+        case let .plain(icon): return icon.rawValue
+        case let .symbol(icon): return icon.rawValue
+        case let .metal(icon): return icon.rawValue
+        }
     }
 
     /// Name used to set alternative app icon
     ///
     /// Should be `nil` for default app icon
     var iconName: String? {
-        switch self {
-        case .plain(.standard): return nil
-        case .plain(.dark): return "AppIconDark"
-        case .plain(.nostalgia): return "AppIconNostalgia"
-        case .plain(.bsuirDonalds): return "AppIconBsuirDonalds"
-        case .symbol(.resist): return "AppIconResist"
-        case .symbol(.national): return "AppIconNational"
-        case .symbol(.ukrainian): return "AppIconUkrainian"
-        case .symbol(.pride): return "AppIconPride"
-        case .metal(.silver): return "AppIconSilver"
-        case .metal(.copper): return "AppIconCopper"
-        }
+        if case .plain(.standard) = self { return nil }
+        return imageName
     }
 }
