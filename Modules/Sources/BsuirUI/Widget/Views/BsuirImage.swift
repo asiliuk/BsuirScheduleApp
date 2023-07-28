29f6785f8cb05676070413c0d66ffd54da212b00
Anton Siliuk <devbudgged@gmail.com>
2023-07-28 15:01:49 +0200

    Fix bsuir logo loading issues

--- Modules/Sources/BsuirUI/Widget/Views/BsuirImage.swift
+++ Modules/Sources/BsuirUI/Widget/Views/BsuirImage.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
 public extension Image {
-    static let bsuirLogo = Image("bsuir.logo.fill", bundle: .module)
+    static let bsuirLogo = Image(uiImage: UIImage.bsuirLogo ?? UIImage())
+}
+
+private extension UIImage {
+    static let bsuirLogo = UIImage(named: "bsuir.logo.fill", in: .module, with: nil)?
+        .withRenderingMode(.alwaysTemplate)
 }
