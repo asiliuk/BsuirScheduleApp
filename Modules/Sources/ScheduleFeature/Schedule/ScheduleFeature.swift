b7e97dc790d9f13cec0711bece91b5468e5ea505
Anton Siliuk <devbudgged@gmail.com>
2023-10-29 14:04:20 +0100

    Move loaded schedule reducer in separate file

--- Modules/Sources/ScheduleFeature/Schedule/ScheduleFeature.swift
+++ Modules/Sources/ScheduleFeature/Schedule/ScheduleFeature.swift
@@ -6,7 +6,6 @@ import ScheduleCore
 import LoadableFeature
 import ComposableArchitecture
 import Dependencies
-import Algorithms
 
 public struct ScheduleRequestResponse {
     public let startDate: Date?
@@ -151,66 +150,11 @@ public struct ScheduleFeature<Value: Equatable>: Reducer {
         .ifLet(\.subgroupPicker, action: /Action.subgroupPicker) {
             SubgroupPickerFeature()
         }
-    }
-}
-
-public struct LoadedScheduleReducer: Reducer {
-    public struct State: Equatable {
-        public var maxSubgroup: Int?
-        var compact: DayScheduleFeature.State
-        var continuous: ContinuousScheduleFeature.State
-        var exams: ExamsScheduleFeature.State
-
-        init(response: ScheduleRequestResponse, pairRowDetails: PairRowDetails?) {
-            self.compact = DayScheduleFeature.State(
-                schedule: response.schedule
-            )
-
-            self.continuous = ContinuousScheduleFeature.State(
-                schedule: response.schedule,
-                startDate: response.startDate,
-                endDate: response.endDate,
-                pairRowDetails: pairRowDetails
-            )
-
-            self.exams = ExamsScheduleFeature.State(
-                exams: response.exams,
-                startDate: response.startExamsDate,
-                endDate: response.endExamsDate,
-                pairRowDetails: pairRowDetails
-            )
-
-            let allSchedulePairs = DaySchedule.WeekDay.allCases
-                .lazy
-                .compactMap { response.schedule[$0] }
-                .flatMap { $0 }
-
-            let allPairs = chain(allSchedulePairs, response.exams)
-
-            self.maxSubgroup = allPairs.lazy
-                .map(\.subgroup)
-                .filter { $0 != 0 }
-                .max()
-        }
-    }
-
-    public enum Action: Equatable {
-        case day(DayScheduleFeature.Action)
-        case continuous(ContinuousScheduleFeature.Action)
-        case exams(ExamsScheduleFeature.Action)
-    }
-
-    public var body: some ReducerOf<Self> {
-        Scope(state: \.compact, action: /Action.day) {
-            DayScheduleFeature()
-        }
-
-        Scope(state: \.continuous, action: /Action.continuous) {
-            ContinuousScheduleFeature()
-        }
-
-        Scope(state: \.exams, action: /Action.exams) {
-            ExamsScheduleFeature()
+        .onChange(of: \.subgroupPicker?.selected) { oldValue, newValue in
+            Reduce { state, action in
+                print("Aaaaaa")
+                return .none
+            }
         }
     }
 }
