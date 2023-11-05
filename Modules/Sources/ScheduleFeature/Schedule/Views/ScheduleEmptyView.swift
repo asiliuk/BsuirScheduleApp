5223ee284cdc7eaaa3462e2d6c5e5976b114546c
Anton Siliuk <devbudgged@gmail.com>
2023-11-05 20:46:37 +0100

    Update empty schedule placeholder

--- Modules/Sources/ScheduleFeature/Schedule/Views/ScheduleEmptyView.swift
+++ Modules/Sources/ScheduleFeature/Schedule/Views/ScheduleEmptyView.swift
@@ -4,22 +4,30 @@ import LoadableFeature
 
 struct ScheduleEmptyView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Image(systemName: imageNames.randomElement()!)
-                .font(.largeTitle)
-            
-            title.font(.title)
-            subtitle.font(.subheadline)
-            
-            Spacer()
+        if #available(iOS 17, *) {
+            ContentUnavailableView(
+                title,
+                systemImage: imageNames.randomElement()!,
+                description: subtitle
+            )
+        } else {
+            VStack {
+                Spacer()
+
+                Image(systemName: imageNames.randomElement()!)
+                    .font(.largeTitle)
+
+                Text(title).font(.title)
+                subtitle.font(.subheadline)
+
+                Spacer()
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text(title) + Text(", ") + subtitle)
         }
-        .accessibilityElement(children: .ignore)
-        .accessibilityLabel(title + Text(", ") + subtitle)
     }
     
-    private let title = Text("screen.schedule.emptyState.title")
+    private let title: LocalizedStringKey = "screen.schedule.emptyState.title"
     private let subtitle = Text("screen.schedule.emptyState.subtitle")
 
     private let imageNames = [
