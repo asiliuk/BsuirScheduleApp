182cbf4bdb3e92fa891c6cee59ff7d9c272bfaa8
Anton Siliuk <devbudgged@gmail.com>
2023-11-10 20:59:58 +0100

    Cache user defaults in memory

--- Modules/Sources/BsuirCore/Persistence/UserDefaults+PersistedValue.swift
+++ Modules/Sources/BsuirCore/Persistence/UserDefaults+PersistedValue.swift
@@ -7,7 +7,7 @@ extension UserDefaults {
         PersistedValue(
             get: { self.dictionary(forKey: key) },
             set: { self.set($0, forKey: key) }
-        )
+        ).cacheInMemory()
     }
 
     public func persistedArray<Element>(
@@ -17,41 +17,41 @@ extension UserDefaults {
         PersistedValue(
             get: { self.array(forKey: key) as? [Element] },
             set: { self.set($0, forKey: key) }
-        )
+        ).cacheInMemory()
     }
 
     public func persistedString(forKey key: String) -> PersistedValue<String?> {
         PersistedValue(
             get: { self.string(forKey: key) },
             set: { self.set($0, forKey: key) }
-        )
+        ).cacheInMemory()
     }
 
     public func persistedInteger(forKey key: String) -> PersistedValue<Int> {
         PersistedValue(
             get: { self.integer(forKey: key) },
             set: { self.set($0, forKey: key) }
-        )
+        ).cacheInMemory()
     }
 
     public func persistedData(forKey key: String) -> PersistedValue<Data?> {
         PersistedValue(
             get: { self.data(forKey: key) },
             set: { self.set($0, forKey: key) }
-        )
+        ).cacheInMemory()
     }
 
     public func persistedBool(forKey key: String) -> PersistedValue<Bool> {
         PersistedValue(
             get: { self.bool(forKey: key) },
             set: { self.set($0, forKey: key) }
-        )
+        ).cacheInMemory()
     }
 
     public func persistedDate(forKey key: String) -> PersistedValue<Date?> {
         PersistedValue(
             get: { self.object(forKey: key) as? Date },
             set: { self.set($0, forKey: key) }
-        )
+        ).cacheInMemory()
     }
 }
