2be369803cc6bbba43a9c2e7a9568f02d6e830ca
Anton Siliuk <devbudgged@gmail.com>
2025-09-22 13:10:20 +0200

    Fix iOS 16 UI missing

--- Modules/Sources/BsuirUI/Extensions/View+OSSpecific.swift
+++ Modules/Sources/BsuirUI/Extensions/View+OSSpecific.swift
@@ -5,6 +5,8 @@ extension View {
     public func bsuirRemovingSidebarToggle() -> some View {
         if #available(iOS 17, *) {
             toolbar(removing: .sidebarToggle)
+        } else {
+            self
         }
     }
 }
@@ -15,6 +17,8 @@ extension View {
     public func bsuirTabbarSidebarAdaptable() -> some View {
         if #available(iOS 18.0, *) {
             tabViewStyle(.sidebarAdaptable)
+        } else {
+            self
         }
     }
 }
