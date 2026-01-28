391b3dbbae7ccbbb097289ae1f32918f5a74e795
Anton Siliuk <devbudgged@gmail.com>
2026-01-28 09:26:02 +0100

    Bump min version to iOS 17

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BsuirScheduleApp",
     defaultLocalization: "en",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "AppFeature",
